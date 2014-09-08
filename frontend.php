215b7395447df67343a6cf122f8df3f6324c35b8
hassan <hassantahir2050@gmail.com>
2014-09-08 17:41:38 +0500

    Make 2 slider

--- frontend.php
+++ frontend.php
@@ -1,4 +1,48 @@
-<?php
-	/*Front End file*/
-
-?>
+<div class="main_home_container">
+	<div class="main_home-banner">
+    	<div id="carwrapper">
+			<div id="carcarousel">
+				<img src="img/large/carnaval1.jpg" alt="c1" width="600" height="400" />
+				<img src="img/large/carnaval2.jpg" alt="c2" width="600" height="400" />
+				<img src="img/large/carnaval3.jpg" alt="c3" width="600" height="400" />
+				<img src="img/large/carnaval4.jpg" alt="c4" width="600" height="400" />
+				<img src="img/large/carnaval5.jpg" alt="c5" width="600" height="400" />
+				<img src="img/large/carnaval6.jpg" alt="c6" width="600" height="400" />
+				<img src="img/large/carnaval7.jpg" alt="c7" width="600" height="400" />
+				<img src="img/large/carnaval8.jpg" alt="c8" width="600" height="400" />
+				<img src="img/large/carnaval9.jpg" alt="c9" width="600" height="400" />
+				<img src="img/large/carnaval10.jpg" alt="c10" width="600" height="400" />
+			</div>
+			<div id="carthumbnails">
+				<img src="img/small/carnaval1.jpg" alt="c1" width="100" height="100" />
+				<img src="img/small/carnaval2.jpg" alt="c2" width="100" height="100" />
+				<img src="img/small/carnaval3.jpg" alt="c3" width="100" height="100" />
+				<img src="img/small/carnaval4.jpg" alt="c4" width="100" height="100" />
+				<img src="img/small/carnaval5.jpg" alt="c5" width="100" height="100" />
+				<img src="img/small/carnaval6.jpg" alt="c6" width="100" height="100" />
+				<img src="img/small/carnaval7.jpg" alt="c7" width="100" height="100" />
+				<img src="img/small/carnaval8.jpg" alt="c8" width="100" height="100" />
+				<img src="img/small/carnaval9.jpg" alt="c9" width="100" height="100" />
+				<img src="img/small/carnaval10.jpg" alt="c10" width="100" height="100" />
+			</div>
+		</div>
+    </div>
+    <div class="collection_banner_style">
+    	<div class="collection_half_div">
+        	<h2>LATEST ISLAMIC COLLECTION</h2>
+            <div class="collection_content">
+            	<div class="collection_slider">
+                    <div id="wrapper">
+                            <div id="carousel" style="width:484px;">
+                            <img src="img/large/rally1.jpg" alt="rally1" width="600" height="400" border="0" />
+                            <img src="img/large/rally2.jpg" alt="rally2" width="600" height="400" border="0" />
+                            <img src="img/large/rally3.jpg" alt="rally3" width="600" height="400" border="0" />
+                            <img src="img/large/rally4.jpg" alt="rally4" width="600" height="400" border="0" />
+                            </div>
+                   		 <div id="pager"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -6,9 +6,69 @@
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
         <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="css/slider_style.css" rel="stylesheet" type="text/css" />
 		<script src="js/jquery-1.10.2.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery.carouFredSel-6.0.4-packed.js"></script>
+        
+        <script type="text/javascript">
+			$(function() {
+				$('#carousel').carouFredSel({
+					items: 1,
+					auto: false,
+					scroll: {
+						fx: 'none'
+					},
+					pagination: {
+						container: '#pager',
+						event: 'mouseenter',
+						anchorBuilder: function(nr) {
+							var src = $(this).attr('src').replace('/large/', '/small/');
+							return '<img src="'+ src +'" border="0" />';
+						}
+					}
+				});
+			});
+		</script>
+        <script type="text/javascript">
+			$(function() {
+				$('#carwrapper').hover(
+					function() {
+						$('#carcarousel').trigger( 'pause' );
+						$('#carthumbnails').parent().animate({
+							top: 280
+						});
+					}, function() {
+						
+						$('#carcarousel').trigger( 'play' );
+						$('#carthumbnails').parent().animate({
+							top: 375
+						});
+					}
+				);
+
+				$('#carcarousel').carouFredSel({
+					scroll: {
+						fx: 'crossfade',
+						onBefore: function( data ) {
+							$('#carthumbnails').trigger( 'slideTo', [ $('#carthumbnails img[alt='+ data.items.visible.attr( 'alt' ) +']'), -2 ] );
+						}
+					}
+				});
+
+				$('#carthumbnails').carouFredSel({
+					auto: false,
+					items: {
+						start: -2
+					}
+				});
+
+				$('#carthumbnails img').click(function() {
+					$('#carcarousel').trigger( 'slideTo', [ $('#carcarousel img[alt='+ $(this).attr( 'alt' ) +']') ] );
+
+				}).css( 'cursor', 'pointer' );
+			});
+		</script>
  	</head>
     <body>
 		<?php include_once("header.php"); ?> 
