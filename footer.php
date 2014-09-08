68ec736965602c2dfd1cf59928a42219fc6dacbb
tahir <tahir@darussalampk.com>
2014-09-08 16:19:39 +0500

    footer + css

--- footer.php
+++ footer.php
@@ -1,4 +1,64 @@
-<?php
 
-	/* Footer Section */
-?>
+<div class="container">
+	<div class="row sign-up-row">
+    	<div class="col-lg-8 sign-up-for-newsletter">
+        	<p><span>Sign up for Newsletter</span> <input type="search" class="sign-up-form" /></p>
+        </div>
+        <div class="col-lg-4 follow-us">
+        	<p><span>Follow me</span> <a href="#" class="twitter"></a> <a href="#" class="facebook"></a> <a href="#" class="google-plus"></a></p>
+        </div>
+    </div>
+</div>
+<div class="container">
+	<div class="footer">
+    	<div class="col-lg-3">
+        	<h3>Our Services</h3>
+            <ul>
+            	<li><a href="#">Lorem Ispum</a></li>
+                <li><a href="#">Lorem Ispum</a></li>
+                <li><a href="#">Lorem Ispum</a></li>
+                <li><a href="#">Lorem Ispum</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3">
+        	<h3>About Us</h3>
+            <ul>
+            	<li><a href="#">Sale</a></li>
+                <li><a href="#">Press</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3">
+        	<h3>Miscellaneous</h3>
+            <ul>
+            	<li><a href="#">Sale</a></li>
+                <li><a href="#">Press</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3">
+        	<h3>Miscellaneous</h3>
+            <p>+(92) 42 37240024</p>
+            <p>support@darussalampk.com</p>
+            <p>Darussalam Publishers, 36 Lower Mall, Secretariat Stop, Lahore, Pakistan</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+	<div class="bottom-footer">
+    	<div class="col-lg-12">
+        	<p>
+            	<a href="#">About Us</a>
+                <a href="#">FAQ's</a>
+                <a href="#">Terms &amp; Conditions</a>
+                <a href="#">Payment Methods</a>
+                <a href="#">Shipment</a>
+                <a href="#">Branches</a>
+                <a href="#">Contact Us</a>
+            </p>
+            <p>&copy; 2014 Darussalam, Inc. All Rights Reserved.</p>
+        </div>
+    </div>
+</div>
