19557fc79fcc79b70de22ea54769baca143d13be
hassan <hassantahir2050@gmail.com>
2014-09-10 17:57:37 +0500

    working make new page of product.php

--- product.php
+++ product.php
@@ -5,21 +5,163 @@
     <link href="css/star-rating.css" media="all" rel="stylesheet" type="text/css"/>
         <link href="css/style.css" rel="stylesheet" type="text/css" />
         <link href="css/slider_style.css" rel="stylesheet" type="text/css" />
+        <link href="css/css_products.css" rel="stylesheet" type="text/css" />
 		<script src="js/jquery-1.10.2.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery.carouFredSel-6.0.4-packed.js"></script>
     <script src="js/star-rating.js" type="text/javascript"></script>
 <body>
 <?php include_once("header.php"); ?>
-<div class="container">
-<form>
+<div class="container product_container">
+	<div class="inner_product_continer">
+    	<div class="product_left_bar">
+        	<div class="product_list_name">
+            	<ul>
+                	<li><a href="#">Islamic Stories</a></li>
+                    <li><a href="#">Collection/Pack</a></li>
+                    <li><a href="#">Salat, Supplication, Repentance</a></li>
+                    <li><a href="#">Children Books</a></li>
+                    <li><a href="#">Worship/Ibadah</a></li>
+                    <li><a href="#">Biography of Prophet</a></li>
+                    <li><a href="#">Lectures/Sermon</a></li>
+                    <li><a href="#">Biography & History</a></li>
+                    <li><a href="#">Aqeedah/Creed</a></li>
+                    <li><a href="#">Dawah/New & Non Muslim</a></li>
+                    <li><a href="#">Fatawa, Fiqh, Islamic Law</a></li>
+                    <li><a href="#">Hadith</a></li>
+                    <li><a href="#">Atlas, Dictionary, Encyclopedia</a></li>
+                    <li><a href="#">Women & Family</a></li>
+                    <li><a href="#">General, Misc</a></li>
+                    <li><a href="#">Islam & Science</a></li>
+                    <li><a href="#">Rights, Duties, Etiquette, Character</a></li>
+                    <li><a href="#">Autobiography & Travelogue</a></li>
+                    <li><a href="#">Comparative Religion</a></li>
+                    <li><a href="#">Learning, Language, Curriculum</a></li>
+                    <li><a href="#">Hereafter/Unseen</a></li>
+                    <li><a href="#">Ramadan, Hajj, Zakat</a></li>
+                    <li><a href="#">Islamic Character & Culture</a></li>
+                </ul>
+            </div>
+            <div class="promotion_banner">
+            	<img src="images/promotion_banner.jpg" alt="promotion">
+            </div>
+        </div>
+        <div class="product_right_part">
+        	<div class="product_main_banner">
+            	<img src="images/product_main_banner.jpg" alt="banner">
+            </div>
+            <div class="book_detail_ecom">
+            	<h2>Collection/Pack</h2>
+                <div class="book_detail_box">
+                	<div class="book_img"><img src="images/book_img.jpg" alt="book"></div>
+                    <div class="book_content">
+                    	<span class="book_description">Islamic Teachings for Women (6-Books Set)</span>
+                        <p>RS: 895 PKR</p>
+                        <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+                        <span class="ebook_style"><img src="images/marker.png"> eBook</span>
+                        <input class="read_button" type="button" value="READ SAMPLE"> <input class="shop_button" type="button" value="SHOP NOW">
+                    </div>
+                    <div class="book_description_2">
+                    	<p><span>Language:</span> English<br><span>Publisher :</span> Darussalam</p>
+                        <p class="right_book_description">Football is not just a beautiful game,<br> but a global obsession. Every four<br> years, football kicks off a frenzy<br> with eve... View More </p>
+                       	<div class="hard_cover_div_style">
+                        	<p>Also available as</p>
+                            <p><span>Hardcover</span> - Rs.912</p>
+                        </div>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="book_detail_box">
+                	<div class="book_img"><img src="images/book_img.jpg" alt="book"></div>
+                    <div class="book_content">
+                    	<span class="book_description">Islamic Teachings for Women (6-Books Set)</span>
+                        <p>RS: 895 PKR</p>
+                        <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+                        <span class="ebook_style"><img src="images/marker.png"> eBook</span>
+                        <input class="read_button" type="button" value="READ SAMPLE"> <input class="shop_button" type="button" value="SHOP NOW">
+                    </div>
+                    <div class="book_description_2">
+                    	<p><span>Language:</span> English<br><span>Publisher :</span> Darussalam</p>
+                        <p class="right_book_description">Football is not just a beautiful game,<br> but a global obsession. Every four<br> years, football kicks off a frenzy<br> with eve... View More </p>
+                       	<div class="hard_cover_div_style">
+                        	<p>Also available as</p>
+                            <p><span>Hardcover</span> - Rs.912</p>
+                        </div>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="book_detail_box">
+                	<div class="book_img"><img src="images/book_img.jpg" alt="book"></div>
+                    <div class="book_content">
+                    	<span class="book_description">Islamic Teachings for Women (6-Books Set)</span>
+                        <p>RS: 895 PKR</p>
+                        <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+                        <span class="ebook_style"><img src="images/marker.png"> eBook</span>
+                        <input class="read_button" type="button" value="READ SAMPLE"> <input class="shop_button" type="button" value="SHOP NOW">
+                    </div>
+                    <div class="book_description_2">
+                    	<p><span>Language:</span> English<br><span>Publisher :</span> Darussalam</p>
+                        <p class="right_book_description">Football is not just a beautiful game,<br> but a global obsession. Every four<br> years, football kicks off a frenzy<br> with eve... View More </p>
+                       	<div class="hard_cover_div_style">
+                        	<p>Also available as</p>
+                            <p><span>Hardcover</span> - Rs.912</p>
+                        </div>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="book_detail_box border_remove">
+                	<div class="book_img"><img src="images/book_img.jpg" alt="book"></div>
+                    <div class="book_content">
+                    	<span class="book_description">Islamic Teachings for Women (6-Books Set)</span>
+                        <p>RS: 895 PKR</p>
+                        <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+                        <span class="ebook_style"><img src="images/marker.png"> eBook</span>
+                        <input class="read_button" type="button" value="READ SAMPLE"> <input class="shop_button" type="button" value="SHOP NOW">
+                    </div>
+                    <div class="book_description_2">
+                    	<p><span>Language:</span> English<br><span>Publisher :</span> Darussalam</p>
+                        <p class="right_book_description">Football is not just a beautiful game,<br> but a global obsession. Every four<br> years, football kicks off a frenzy<br> with eve... View More </p>
+                       	<div class="hard_cover_div_style">
+                        	<p>Also available as</p>
+                            <p><span>Hardcover</span> - Rs.912</p>
+                        </div>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="bottom_collection_detail">
+    	<h3>WHAT OTHER CUSTOMERS ARE LOOKING AT?</h3>
+        <div class="collection_images">
+        	<figure>
+            	<img src="images/b_img_1.jpg" alt="image" />
+            	<figcaption>Quran Collection</figcaption>
+            </figure>
+            <figure>
+            	<img src="images/b_img_2.jpg" alt="image" />
+            	<figcaption>Women Collection</figcaption>
+            </figure>
+            <figure>
+            	<img src="images/b_img_3.jpg" alt="image" />
+            	<figcaption>Toys for Kids</figcaption>
+            </figure>
+            <figure>
+            	<img src="images/b_img_4.jpg" alt="image" />
+            	<figcaption>Abe-e-zam zam</figcaption>
+            </figure>
+        </div>
+    </div>
+</div>
+
+<!--<form>
     <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
     <br />
     <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
     <br />
     <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
-</form>
-<br>
+</form>-->
 <script>
     jQuery(document).ready(function () {
         $("#input-21f").rating({
