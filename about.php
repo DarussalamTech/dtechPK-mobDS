2c7f5e1658d0d0e8d4b34cdad522376f24e49c28
tahir <tahir@darussalampk.com>
2014-09-25 17:11:47 +0500

    about us

--- about.php
+++ about.php
@@ -15,10 +15,10 @@
 	<div id="about-us">
         <h2>About Us</h2>
         <p><span>Darussalam is a Multilingual International Islamic Publishing House, headquartered in Riyadh, Kingdom of Saudi Arabia, and branches and agents in major countries around the world. The foremost obligation of Darussalam is to publish authentic Islamic books in the light of the Quran and the Sahih Ahadith in all major International languages.</span></p>
-        <p><span>TTo impart and impel the above mentioned sacred obligation, Darussalam has been engaged, from inception, in producing books on Islam in Arabic, English, Urdu, Spanish, French, Hindi, Persian, Malayalam, Turkish, Indonesian, Russian, Albanian and Bangla languages. The main theme of these books is to present to the reader the fundamentals of Islam as explained by the most recognized Islamic Scholars in the Muslim world.</span></p>
+        <p><span>To impart and impel the above mentioned sacred obligation, Darussalam has been engaged, from inception, in producing books on Islam in Arabic, English, Urdu, Spanish, French, Hindi, Persian, Malayalam, Turkish, Indonesian, Russian, Albanian and Bangla languages. The main theme of these books is to present to the reader the fundamentals of Islam as explained by the most recognized Islamic Scholars in the Muslim world.</span></p>
         <p><span>The other main features of Darussalam are as follows:</span></p>
         <ol>
-            <li><span>Presenting books free from sectarianism and in accordance with the Qur&acute;an and the Sunnah.</span></li>
+            <li><span>Presenting books free from sectarianism and in accordance with the Quran and the Sunnah.</span></li>
             <li><span>Producing books in concise, easy, lucid, comprehensive form.</span></li>
             <li><span>Keeping the prices of the books less than the global market prices.</span></li>
             <li><span>Maintaining the quality of books according to the international standard.</span></li>
@@ -26,7 +26,7 @@
             <li><span>Presenting books written by the most senior Islamic scholars &amp; authors.</span></li>
             <li><span>Editing of manuscripts by a Board of senior editors.</span></li>
             <li><span>Supervising every stage of publication by a team of professional technical staff.</span></li>
-    </ol>
+    	</ol>
     </div>
 </div>
 <?php include_once("footer.php"); ?>  
